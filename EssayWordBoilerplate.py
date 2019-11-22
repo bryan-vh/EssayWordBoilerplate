74fd059c41d75f3162c67abd3ab7411455291a2c
Bryan Van Horn <bvhwa@yahoo.com>
2019-11-22 01:27:57 -0800

    Initial commit

---
+++ EssayWordBoilerplate.py
@@ -0,0 +1,73 @@
+import docx
+import os
+import calendar
+
+from docx.shared import Pt
+from datetime import datetime
+
+
+def generateBoilerplate(student, professor, class_name, title, date):
+    runValue = {
+        0: student,
+        1: professor,
+        2: class_name,
+        3: date,
+        4: title
+    }
+
+    doc = docx.Document()
+
+    paragraphs = []
+
+    for i in range(5):
+        paragraphs.append(doc.add_paragraph())
+
+    for i, paragraph in enumerate(paragraphs):
+        format = paragraph.paragraph_format
+        format.space_before = Pt(0)
+        format.space_after = Pt(0)
+        format.line_spacing = 2
+
+        if i == 4:
+            format.alignment = 1
+
+        run = paragraph.add_run(runValue[i])
+        font = run.font
+
+        font.size = Pt(12)
+        font.name = 'Times New Roman'
+
+    documents_folder = os.path.expanduser('~/Documents/')
+    file = input("What do you want to call this .docx file? ")
+
+    path = documents_folder + file + '.docx'
+
+    doc.save(path)
+
+
+def getDateString():
+    now = datetime.now()
+
+    day = now.date().day
+    month = calendar.month_name[now.date().month]
+    year = now.date().year
+
+    date = '{day} {month} {year}'.format(day=day, month=month, year=year)
+
+    print(date)
+
+    return date
+
+
+def main():
+    student = input('What is your full name? ')
+    professor = 'Professor ' + input('What is your professor\'s last name? ')
+    class_name = input('What is the class name (preferably abbreviated version)? ')
+    title = input('What is the title of the paper? ')
+    date = getDateString()
+
+    generateBoilerplate(student, professor, class_name, title, date)
+
+
+if __name__ == '__main__':
+    main()
